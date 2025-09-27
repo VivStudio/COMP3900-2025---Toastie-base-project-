2774807b7df404d0350002c67dba25d391eaf436
Viv <vivianshen046110@gmail.com>
2025-09-27 15:14:18 +1000

    update instructions

--- Toastie/client/lib/services/supabase/key.dart
+++ Toastie/client/lib/services/supabase/key.dart
@@ -1,6 +1,5 @@
-final String supabaseUrl = 'https://ljumoqywmjdjqamsjgdg.supabase.co';
-final String supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqdW1vcXl3bWpkanFhbXNqZ2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcwMDIzNjUsImV4cCI6MjAyMjU3ODM2NX0.iuYMwViO7-6pgGtWtulveB50kuJroz4qiC2dze9kbMU';
+final String supabaseUrl = 'TODO';
+final String supabaseKey = 'TODO';
 
 // Authentication ids
 final String oauthWebClientId = 'TODO';
